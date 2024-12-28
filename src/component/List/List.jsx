feeaadbdaa432d58ceeb32177ddb91a725d23dce
paulSuman005 <sumanpauls568gmail.com>
2024-12-29 01:04:02 +0530

    add List component

---
+++ src/component/List/List.jsx
@@ -0,0 +1,12 @@
+
+function List({ items }) {
+    return (
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    );
+  }
+  
+  export default List;
